42e50d1aacc226e1a1268130121c3b01fe973bbc
Mykola Symon <simonnikolaj20@gmail.com>
2022-12-27 17:36:39 +0300

    a very big and important update

--- bot/connector.py
+++ bot/connector.py
@@ -63,7 +63,7 @@ class Connector:
                 f"avgPrice: {item['info']['avgPx']}\n"
                 f"unrealizedPnl: {item['unrealizedPnl']} ({round(item['percentage'], 2)}%)\n"
                 f"liquidationPrice: {item['liquidationPrice']}\n"
-                f"Pos size: {item['info']['notionalUsd']}💰"
+                f"Pos size: {item['info']['notionalUsd']}💰\n"
             )
 
         return result
