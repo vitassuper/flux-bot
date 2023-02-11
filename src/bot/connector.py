5736442360c706e5378cd22d305473ef46481ff0
vitassuper <vitassuper99@gmail.com>
2023-02-11 12:10:42 +0000

    Refactor bot/connector

--- src/bot/connector.py
+++ src/bot/connector.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Tuple, Union
 import ccxt
 
 from datetime import datetime
@@ -6,10 +6,13 @@ from src.app.schemas.deals import DealCreate, DealUpdate
 from src.app import schemas
 
 from src.app.services.deal import create_deal, get_deal_by_exchange_id, get_opened_deals, increment_safety_orders_count, update_deal_by_exchange_id
+from src.bot.exception import ConnectorException
 from src.bot.notificator import Notificator
 from decimal import Decimal, ROUND_DOWN
+from src.bot.helper import calculate_position_pnl_for_position, get_time_duration_string
 
 from src.core.config import settings
+
 
 class Connector:
     def __init__(self):
@@ -28,215 +31,193 @@ class Connector:
 
         self.okx.load_markets()
 
-    def dispatch(self, signal: Union[schemas.AddSignal, schemas.OpenSignal, schemas.CloseSignal]):
-        match signal.type_of_signal:
-            case 'open':
-                connector.open_short_position(signal.pair, signal.amount)
-            case 'close':
-                connector.close_short_position(signal.pair)
-            case 'add':
-                connector.add_to_short_position(signal.pair, signal.amount)
+    def dispatch(self, signal: Union[schemas.AddSignal, schemas.OpenSignal, schemas.CloseSignal]) -> None:
+        try:
+            match signal.type_of_signal:
+                case 'open':
+                    connector.dispatch_open_short_position(
+                        signal.pair, signal.amount)
+                case 'close':
+                    connector.dispatch_close_short_position(signal.pair)
+                case 'add':
+                    connector.dispatch_add_to_short_position(
+                        signal.pair, signal.amount)
+                case _:
+                    raise ConnectorException('unknown type of signal')
 
+        except ConnectorException as e:
+            self.notificator.send_warning_notification(
+                f"Cant {signal.type_of_signal}: {str(e)}")
+            pass
 
-    def get_position(self, pair):
+    def get_position(self, pair: str):
         positions = self.okx.fetch_positions()
 
-        return next((p for p in positions if p["info"]["instId"] == pair), None)
+        open_position = next(
+            (p for p in positions if p["info"]["instId"] == pair), None)
 
-    def convert_quote_to_contracts(self, symbol, amount):
+        if not open_position:
+            raise ConnectorException('position not exists')
+
+        return open_position
+
+    def convert_quote_to_contracts(self, symbol: str, amount: float) -> Tuple[int, float]:
         market = self.okx.market(symbol)
         price = self.okx.fetch_ticker(symbol)["last"]
 
-        contractsSize = int(amount / price / market["contractSize"])
+        contracts_size = int(amount / price / market["contractSize"])
 
-        contractsCost = contractsSize * price * market["contractSize"]
+        if not contracts_size:
+            raise ConnectorException(f"low amount for pair: {symbol}")
 
-        return contractsSize, contractsCost
+        contracts_cost = contracts_size * price * market["contractSize"]
 
-    def findElement(self, items, function):
-        for item in items:
-            if function(item):
-                return item
+        return contracts_size, contracts_cost
 
-    def get_open_positions(self):
+    def get_open_positions_info(self):
         positions = self.okx.fetch_positions()
         positions.sort(key=lambda item: item["info"]["instId"])
 
         deals = get_opened_deals()
 
-        tickers = self.okx.fetch_tickers([item['info']['instId'] for item in positions if 'info' in item and 'instId' in item['info']])
+        tickers = self.okx.fetch_tickers(
+            [item['info']['instId'] for item in positions if 'info' in item and 'instId' in item['info']])
 
         result = ""
         for item in positions:
-            deal = self.findElement(
-                deals, (lambda deal: deal.pair == item["info"]["instId"])
-            )
+            deal = next((x for x in deals if x.pair ==
+                        item["info"]["instId"]), None)
 
             symbol = item['symbol']
 
             result += (
                 f"{item['info']['instId']}\n"
                 f"margin: {Decimal(item['info']['margin']).quantize(Decimal('0.001'))}\n"
-                f"currentPrice: {tickers[item['symbol']]['last']}\n"
+                f"currentPrice: {tickers[symbol]['last']}\n"
                 f"avgPrice: {self.okx.price_to_precision(symbol, item['info']['avgPx'])}\n"
                 f"unrealizedPnl: {Decimal(item['unrealizedPnl']).quantize(Decimal('0.0001'))} ({round(item['percentage'], 2)}%)\n"
                 f"liquidationPrice: {self.okx.price_to_precision(symbol, item['liquidationPrice'])}\n"
                 f"Pos size: {Decimal(item['info']['notionalUsd']).quantize(Decimal('0.001'))}💰\n"
-                f"Duration: {self.get_time_duration(deal.date_open, datetime.now()) if deal else 'Unknown info'}\n"
-                f"Safety orders {deal.safety_order_count if deal else 'Unknown info'}\n\n"
             )
+
+            if (deal):
+                result += (
+                    f"Duration: {get_time_duration_string(deal.date_open, datetime.now())}\n"
+                    f"Safety orders {deal.safety_order_count}\n\n"
+                )
 
         return result
 
-    def open_short_position(self, pair, amount, leverage=20):
+    def dispatch_open_short_position(self, pair: str, amount: float):
         self.notificator.send_notification(
-            f"Received open signal: pair: {pair}, amount: {amount}"
-        )
-
-        quantity, contractsCost = self.convert_quote_to_contracts(pair, amount)
-
-        if quantity == 0:
-            self.notificator.send_warning_notification(
-                f"Can't open new position, low amount for pair: {pair}"
-            )
-
-            return
+            f"Received open signal: pair: {pair}, amount: {amount}")
 
         if self.get_position(pair):
-            self.notificator.send_warning_notification(
-                f"Can't open new position, position already exists pair: {pair}"
-            )
+            raise ConnectorException(f"position already exists pair: {pair}")
 
-            return
+        quantity, contracts_cost = self.convert_quote_to_contracts(
+            pair, amount)
 
+        self.set_leverage_for_short_position(pair, 20)
+
+        self.sell_short_position(pair, quantity)
+
+        self.add_margin_to_short_position(pair, contracts_cost * 0.06)
+
+        open_position = self.get_position(pair)
+
+        create_deal(DealCreate(
+            pair=pair, exchange_id=open_position["info"]["posId"], date_open=datetime.now()))
+
+        self.notificator.send_notification(
+            f"Opened position: {pair}, amount: {amount}")
+
+    def dispatch_add_to_short_position(self, pair: str, amount: float):
+        self.notificator.send_notification(
+            f"Received signal type: add, pair: {pair}, amount: {amount}")
+
+        open_position = self.get_position(pair)
+
+        quantity, contracts_cost = self.convert_quote_to_contracts(
+            pair, amount)
+
+        self.sell_short_position(pair, quantity)
+
+        self.add_margin_to_short_position(pair, contracts_cost * 0.06)
+
+        deal = increment_safety_orders_count(open_position["info"]["posId"])
+
+        self.notificator.send_notification(
+            f"Averaged position, pair: {pair}, amount: {amount}")
+
+    def dispatch_close_short_position(self, pair: str):
+        self.notificator.send_notification(
+            f"Received signal type: close, pair: {pair}")
+
+        open_position = self.get_position(pair)
+
+        order = self.buy_short_position(pair, open_position["contracts"])
+
+        result = self.okx.fetch_order(order["id"], symbol=pair)
+
+        deal = get_deal_by_exchange_id(open_position["info"]["posId"])
+
+        update_deal_by_exchange_id(open_position["info"]["posId"], DealUpdate(
+            pnl=result["info"]["pnl"], date_close=datetime.fromtimestamp(result["timestamp"]/1000)))
+
+        pnl = Decimal(result["info"]["pnl"]).quantize(
+            Decimal("0.0001"), rounding=ROUND_DOWN
+        )
+
+        pnl_percentage = calculate_position_pnl_for_position(
+            result["price"], open_position["entryPrice"], float(
+                result["info"]["lever"]), result["info"]["posSide"]
+        )
+
+        duration = get_time_duration_string(
+            deal.date_open, datetime.fromtimestamp(result["timestamp"] / 1000))
+
+        self.notificator.send_notification((
+            f"{result['symbol']}\n"
+            f"Profit:{pnl}$ ({pnl_percentage}%)\n"
+            f"Size: {result['info']['fillSz']}\n"
+            f"Duration: {duration}"
+        ))
+
+    def add_margin_to_short_position(self, pair: str, amount: float):
+        self.okx.add_margin(symbol=pair, amount=amount,
+                            params={"posSide": "short"})
+
+    def set_leverage_for_short_position(self, pair: str, leverage: int):
         self.okx.set_leverage(
             leverage=leverage,
             symbol=pair,
             params={"mgnMode": "isolated", "posSide": "short"},
         )
 
-        order = self.okx.create_order(
+    def sell_short_position(self, pair: str, amount: int):
+        self.okx.create_order(
             symbol=pair,
             side="sell",
             type="market",
-            amount=quantity,
+            amount=amount,
             params={
                 "posSide": "short",
                 "tdMode": "isolated",
             },
         )
 
-        open_position = self.get_position(pair)
-
-        create_deal(DealCreate(pair=pair, exchange_id=open_position["info"]["posId"], date_open=datetime.now()))
-
-        self.okx.add_margin(symbol=pair, amount=contractsCost * 0.06, params={"posSide": "short"})
-
-    def add_to_short_position(self, pair, amount):
-        self.notificator.send_notification(
-            f"Received signal type: add, pair: {pair}, amount: {amount}"
-        )
-
-        open_position = self.get_position(pair)
-
-        if not open_position:
-            self.notificator.send_warning_notification(
-                f"Can't average position, position not exists pair: {pair}"
-            )
-
-            return
-
-        quantity, contractsCost = self.convert_quote_to_contracts(pair, amount)
-
-        if quantity == 0:
-            self.notificator.send_warning_notification(
-                f"Can't average position, low amount for pair: {pair}"
-            )
-
-            return
-
-        order = self.okx.create_order(
-            symbol=pair,
-            side="sell",
-            type="market",
-            amount=quantity,
-            params={
-                "posSide": "short",
-                "tdMode": "isolated",
-            },
-        )
-
-        self.okx.add_margin(symbol=pair, amount=contractsCost * 0.06, params={"posSide": "short"})
-
-        deal = increment_safety_orders_count(open_position["info"]["posId"])
-
-        self.notificator.send_notification(
-            f"Averaged position ({deal.safety_order_count})"
-        )
-
-    def close_short_position(self, pair):
-        self.notificator.send_notification(f"Received signal type: close, pair: {pair}")
-
-        open_position = self.get_position(pair)
-
-        if not open_position:
-            self.notificator.send_warning_notification(
-                f"Can't close position, position not exists pair: {pair}"
-            )
-
-            return
-
-        order = self.okx.create_order(
+    def buy_short_position(self, pair: str, amount: int):
+        return self.okx.create_order(
             symbol=pair,
             side="buy",
             type="market",
-            amount=int(open_position["contracts"]),
+            amount=amount,
             params={
                 "posSide": "short",
                 "tdMode": "isolated",
             },
         )
 
-        result = self.okx.fetch_order(order["id"], symbol=pair)
-
-        deal = get_deal_by_exchange_id(open_position["info"]["posId"])
-
-        update_deal_by_exchange_id(open_position["info"]["posId"], DealUpdate(pnl=result["info"]["pnl"], date_close=datetime.fromtimestamp(result["timestamp"]/1000)))
-
-        pnl = Decimal(result["info"]["pnl"]).quantize(
-            Decimal("0.0001"), rounding=ROUND_DOWN
-        )
-
-        pnl_percentage = self.calculate_pnl_percentage(
-            result["price"], open_position["entryPrice"], float(result["info"]["lever"]), result["info"]["posSide"]
-        )
-
-        duration = self.get_time_duration(deal.date_open, datetime.fromtimestamp(result["timestamp"] / 1000))
-
-        self.notificator.send_notification(
-            (
-                f"{result['symbol']}\n"
-                f"Profit:{pnl}$ ({pnl_percentage}%)\n"
-                f"Size: {result['info']['fillSz']}\n"
-                f"Duration: {duration}"
-            )
-        )
-
-    def get_time_duration(self, date_open: datetime, date_close: datetime) -> str:
-        diff = date_close - date_open
-        seconds_duration = diff.total_seconds()
-
-        days, seconds = divmod(seconds_duration, 86400)
-        hours, seconds = divmod(seconds_duration, 3600)
-        minutes, seconds = divmod(seconds_duration, 60)
-
-        hours, minutes = divmod(minutes, 60)
-
-        return f"{int(days)} days, {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds"
-
-    def calculate_pnl_percentage(self, closePrice, avgPrice, leverage, posSide):
-        return Decimal(
-            ((closePrice - avgPrice) / avgPrice) * float(leverage) * 100 * (-1 if posSide == "short" else 1)
-        ).quantize(Decimal("0.01"))
 
 connector = Connector()
---
+++ src/bot/helper.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from decimal import Decimal
+
+from src.bot.exception import ConnectorException
+
+
+def get_time_duration_string(date_open: datetime, date_close: datetime) -> str:
+    diff = date_close - date_open
+    seconds_duration = diff.total_seconds()
+
+    days, seconds = divmod(seconds_duration, 86400)
+    hours, seconds = divmod(seconds_duration, 3600)
+    minutes, seconds = divmod(seconds_duration, 60)
+
+    hours, minutes = divmod(minutes, 60)
+
+    return f"{int(days)} days, {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds"
+
+
+def calculate_pnl_percentage(closePrice, avgPrice):
+    return Decimal(
+        ((closePrice - avgPrice) / avgPrice) * 100
+    ).quantize(Decimal("0.01"))
+
+
+def calculate_position_pnl_percentage(closePrice, avgPrice, leverage):
+    return calculate_pnl_percentage(closePrice, avgPrice) * float(leverage)
+
+
+def calculate_position_pnl_for_position(close_price, avg_price, leverage, pos_side):
+    return calculate_position_pnl_percentage(close_price, avg_price, leverage) * get_percentage_sign(pos_side)
+
+
+def calculate_pnl_for_position(close_price, avg_price, pos_side):
+    return calculate_pnl_percentage(close_price, avg_price) * get_percentage_sign(pos_side)
+
+
+def get_percentage_sign(pos_side):
+    if pos_side == "short":
+        return -1
+    if pos_side == "long":
+        return 1
+
+    raise ConnectorException('undefined position direction')
