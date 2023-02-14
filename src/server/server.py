f5d5fb0354f4a376212d0ef181ff9bf0f221b685
vitassuper <vitassuper99@gmail.com>
2023-02-14 09:45:45 +0000

    Send validation error to telegram

--- src/server/server.py
+++ src/server/server.py
@@ -1,18 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from src.bot.notificator import Notificator
 from src.core.config import settings
 from starlette.middleware.cors import CORSMiddleware
 from src.app.routes import signal
 from src.bot.connector import connector
+from fastapi.exceptions import RequestValidationError
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    notificator = Notificator()
+    notificator.send_warning_notification(f"{exc}")
+
+    return Response(content="Validation Error", status_code=400)
+
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=[str(origin)
+                       for origin in settings.BACKEND_CORS_ORIGINS],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
