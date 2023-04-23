910a5cf83b40b344718b5f3311acc78012002059
vitassuper <vitassuper99@gmail.com>
2023-04-23 21:48:58 +0000

    Add created_at column to orders

--- src/app/models/order.py
+++ src/app/models/order.py
@@ -1,6 +1,7 @@
 import decimal
+from datetime import datetime
 
-from sqlalchemy import ForeignKey, String, Numeric
+from sqlalchemy import ForeignKey, String, Numeric, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.app.models.base import Base
@@ -14,3 +15,4 @@ class Order(Base):
     side: Mapped[str] = mapped_column(String(10), nullable=False)
     price: Mapped[decimal] = mapped_column(Numeric(precision=20, scale=10), nullable=False)
     volume: Mapped[decimal] = mapped_column(Numeric(precision=20, scale=10), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(), nullable=True, default=datetime.now())
